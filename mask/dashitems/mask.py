f68a8fe5101c1724b4871b842dcb602524940d03
Nikhil Jothi Prakash <nik463@gmail.com>
2022-01-31 12:14:48 +0530

    ready to deploy fist ver

--- mask/dashitems/mask.py
+++ mask/dashitems/mask.py
@@ -32,28 +32,5 @@ app.layout = html.Div(
         html.Br(),
 
                
-    ]
-)
-
-    html.Div([
-    dcc.Upload(
-        id='upload-image',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-image-upload'),
-])
+    ],
+),
