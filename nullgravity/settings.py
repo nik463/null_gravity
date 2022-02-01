f77b5c71bf33fe64cbcc0c9258a90d2b5d0d4d7c
Nikhil Jothi Prakash <nik463@gmail.com>
2022-02-01 23:04:22 +0530

    add new heroku setting asssets

--- nullgravity/settings.py
+++ nullgravity/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
