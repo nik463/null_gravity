cab18dafa306bbf4930ee530e27965f574d6a127
Nikhil Jothi Prakash <nik463@gmail.com>
2022-02-02 17:12:35 +0530

    django deploy

--- nullgravity/settings.py
+++ nullgravity/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'nullgravity.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'template'],
+        'DIRS': [BASE_DIR/'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
